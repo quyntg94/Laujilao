d382edbfca6c13eba2c68075e57666616a5233c2
quynth94 <giaquy.ptit@gmail.com>
2017-05-19 12:02:05 +0700

    commit

--- app.py
+++ app.py
@@ -6,17 +6,17 @@ import os
 
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-GUESTHOUSE_PATH = os.path.join(APP_ROOT, "guesthouse.json")
+FOOD_PATH = os.path.join(APP_ROOT, "food.json")
 
 app = Flask(__name__)
 api = Api(app)
 
-class GuesthouseListRes(Resource):
+class FoodListRes(Resource):
     def get(self):
-        with codecs.open(GUESTHOUSE_PATH, 'r', 'utf-8-sig') as data_file:
+        with codecs.open(FOOD_PATH, 'r', 'utf-8-sig') as data_file:
             return json.load(data_file)
 
-api.add_resource(GuesthouseListRes, "/api/guesthouse")
+api.add_resource(FoodListRes, "/api/food")
 
 if __name__ == '__main__':
     app.run()

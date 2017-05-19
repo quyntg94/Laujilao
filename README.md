6c6ed398555bfe9ac5e66c9dbcfdabdd6fd3c128
quynth94 <giaquy.ptit@gmail.com>
2017-05-19 11:49:15 +0700

    commit

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 # Demo
-=======
-# laujilao
->>>>>>> 71c483e0494e2ff7cfef086d305beaa1ec3430ee
-=======
-# Laujilao
->>>>>>> 10fbfd6ead1d1e7ac1176f7b561dc47e157b92ad
--- app.py
+++ app.py
@@ -4,18 +4,19 @@ import json
 import codecs
 import os
 
+
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-FOOD_PATH = os.path.join(APP_ROOT, "food.json")
+GUESTHOUSE_PATH = os.path.join(APP_ROOT, "guesthouse.json")
 
 app = Flask(__name__)
 api = Api(app)
 
-class FoodListRes(Resource):
+class GuesthouseListRes(Resource):
     def get(self):
-        with codecs.open(FOOD_PATH, 'r', 'utf-8-sig') as data_file:
+        with codecs.open(GUESTHOUSE_PATH, 'r', 'utf-8-sig') as data_file:
             return json.load(data_file)
 
-api.add_resource(FoodListRes, "/api/food")
+api.add_resource(GuesthouseListRes, "/api/guesthouse")
 
 if __name__ == '__main__':
     app.run()
